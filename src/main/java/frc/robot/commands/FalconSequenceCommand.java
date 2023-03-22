b30142d0cf5432f8686643c9fcd8f0f100865c85
FRC IHS <imperialfrc8119@gmail.com>
2023-03-22 07:34:29 -0700

    waitCommand added

--- src/main/java/frc/robot/commands/FalconSequenceCommand.java
+++ src/main/java/frc/robot/commands/FalconSequenceCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.FalconSubsystem;
 
 public class FalconSequenceCommand {
@@ -8,6 +9,7 @@ public class FalconSequenceCommand {
 
     public FalconSequenceCommand(FalconSubsystem falconSubsystem, double setpoint) {
         m_commandGroup = new SequentialCommandGroup(
+            new WaitCommand(5),
             new MoveToFalconSetpointCommand(falconSubsystem, setpoint).withTimeout(5),
             new FalconStopCommand(falconSubsystem)
         );
